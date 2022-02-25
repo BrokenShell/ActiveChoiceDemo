a7e3f5f52df407d75bc988178bfb7c1c6f0df0c0
BrokenShell <webmaster@sharpdesigndigital.com>
2022-02-24 19:52:11 -0800

    updates main script

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from active_choice import ActiveChoice
 from game import run_game
 
 
-player_1 = ActiveChoice("Player 1", ("lizard", "scissors", "rock", "paper", "spock"))
-player_2 = ActiveChoice("Player 2", ("lizard", "scissors", "rock"))
+player_1 = ActiveChoice("Player 1", ("rock", "paper", "scissors", "lizard", "spock"))
+player_2 = ActiveChoice("Player 2", ("rock", "paper", "scissors"))
 
 
 print("\n# Pre Game")
 print(player_1)
 print(player_2)
 
-run_game(player_1, player_2, cycles=10000)
+run_game(player_1, player_2, cycles=1000)
 
 print("\n# Final Scores")
 print(player_1)
